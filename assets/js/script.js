3311a2b61e5d2bcc3c20a47d85dc613c930aaf23
Stiouf <stephane.quentin@live.fr>
2018-10-05 12:04:55 +0200

    ex4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function ex4(){
-  var lstname = "Nom : " + document.getElementById("lastname").value;
-  var frstname = "Prénom : " + document.getElementById("firstname").value;
-  var cty = "Ville : " + document.getElementById("city").value;
-  alert(lstname +'\n' + frstname +'\n' + cty);
+  var lstname = "Nom : " + document.getElementById("lastname").value; //variable lstname qui met le texte nom : et récupére l'info' de l'id lastname tapé auparavant
+  var frstname = "Prénom : " + document.getElementById("firstname").value; //idem
+  var cty = "Ville : " + document.getElementById("city").value; //idem
+  alert(lstname +'\n' + frstname +'\n' + cty); // affiche une boite de dialogue avec toutes les variables à la ligne
 }
