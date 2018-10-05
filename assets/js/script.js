d7100620a3c02697da66575e8895694be93b6cac
Stiouf <stephane.quentin@live.fr>
2018-10-05 11:49:39 +0200

    EX4JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function ex4(){
-  var lstname = "Nom : "+ document.getElementById("lastname").value;
-  var frstname = "Prénom : "+document.getElementById("firstname").value;
-  var cty = "Ville : "+document.getElementById("city").value;
+  var lstname = "Nom : " + document.getElementById("lastname").value;
+  var frstname = "Prénom : " + document.getElementById("firstname").value;
+  var cty = "Ville : " + document.getElementById("city").value;
   alert(lstname +'\n' + frstname +'\n' + cty);
 }
