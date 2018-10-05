c2b9d040c23b2ce1110495b5673f460cf3628218
Stiouf <stephane.quentin@live.fr>
2018-10-05 11:46:50 +0200

    EX4JS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function ex4(){
+  var lstname = "Nom : "+ document.getElementById("lastname").value;
+  var frstname = "Prénom : "+document.getElementById("firstname").value;
+  var cty = "Ville : "+document.getElementById("city").value;
+  alert(lstname +'\n' + frstname +'\n' + cty);
+}
