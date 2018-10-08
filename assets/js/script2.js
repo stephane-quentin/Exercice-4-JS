58f04358d0d79e4cd1d883210c506e1afa8158a9
Stiouf <stephane.quentin@live.fr>
2018-10-08 11:04:19 +0200

    correction

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,8 +1,9 @@
-function ex4(){
+var bouton = document.querySelector("button"); //sans modifier l'index.html, une variable qui définit le button de la page html
+bouton.onclick = function() { //Au clic sur le button, la fonction est éxécutée
   var lstName = document.getElementById("lastname").value; //variable lstname qui met le texte nom : et récupére l'info' de l'id lastname tapé auparavant
   var frstName = document.getElementById("firstname").value; //idem
   var cty = document.getElementById("city").value; //idem
-  var result = "Nom : " + lstName + '\n' + "Prénom : " + frstName + '\n' + "Ville : " + cty;
+  var result = 'Nom : ' + lstName + '\n' + 'Prénom : ' + frstName + '\n' + 'Ville : ' + cty;
 
   if(isNaN(lstName) && isNaN(frstName) && isNaN(cty)){ //condition isNan pour vérifer que ce n'est pas un nombre
   alert(result); // affiche une boite de dialogue avec toutes les variables à la ligne
